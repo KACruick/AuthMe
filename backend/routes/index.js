903ca481883951796b5aeaaf4e32a11f89838412
Kendra <cruickshank.kendra@gmail.com>
2024-10-08 17:18:51 -0700

    added a .get("/")

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,6 +4,10 @@ const apiRouter = require("./api/index");
 
 router.use("/api", apiRouter);
 
+router.get("/", (res,req) => {
+  res.send("Hello World");
+})
+
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
   res.cookie("XSRF-TOKEN", csrfToken);
