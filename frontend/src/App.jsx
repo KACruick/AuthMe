e345b054f9cdabcf2ecb9569907f8130372a1f77
Kendra <cruickshank.kendra@gmail.com>
2024-11-21 17:00:04 -0800

    added frontend AuthMe

---
+++ frontend/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import SignupFormPage from './components/SignupFormModal/SignupFormModal.jsx';
+import Navigation from './components/Navigation/Navigation.jsx';
+import * as sessionActions from './store/session.js';
+
+function Layout() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => {
+      setIsLoaded(true)
+    });
+  }, [dispatch]);
+
+  return (
+    <>
+      <Navigation isLoaded={isLoaded} />
+      {isLoaded && <Outlet />}
+    </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <h1>Welcome!</h1>
+      },
+      {
+        path: 'login',
+        element: <LoginFormPage />
+      },
+      {
+        path: 'signup',
+        element: <SignupFormPage />
+      },
+    ]
+  }
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
